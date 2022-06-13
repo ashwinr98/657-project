36cd20595b04745a6380d224c3f8a00fbecb1376
Ashwin <ashwinrajasankar@gmail.com>
2022-06-13 12:18:22 -0400

    changed login screen

--- App.js
+++ App.js
@@ -9,87 +9,36 @@ import {
   Button,
   TouchableOpacity,
 } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import Login from "./screens/login";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
  
+const Stack = createNativeStackNavigator();
 export default function App() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
- 
   return (
-    <View style={styles.container}>
-      <Image style={styles.image} source={require("./assets/logo.jpg")} />
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={navStyling}>
+        <Stack.Screen name="Login" component={Login} />
  
-      <StatusBar style="auto" />
-      <View style={styles.inputView}>
-        <TextInput
-          style={styles.TextInput}
-          placeholder="Email."
-          placeholderTextColor="#003f5c"
-          onChangeText={(email) => setEmail(email)}
-        />
-      </View>
- 
-      <View style={styles.inputView}>
-        <TextInput
-          style={styles.TextInput}
-          placeholder="Password."
-          placeholderTextColor="#003f5c"
-          secureTextEntry={true}
-          onChangeText={(password) => setPassword(password)}
-        />
-      </View>
- 
-      <TouchableOpacity>
-        <Text style={styles.forgot_button}>Forgot Password?</Text>
-      </TouchableOpacity>
- 
-      <TouchableOpacity style={styles.loginBtn}>
-        <Text style={styles.loginText}>LOGIN</Text>
-      </TouchableOpacity>
-    </View>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
  
+const navStyling = {
+  headerStyle: {
+    backgroundColor: "#0065A4",
+  },
+  headerTintColor: "#fff",
+  headerTitleStyle: {
+    fontWeight: "bold",
+  },
+};
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
- 
-  image: {
-    marginBottom: 40,
-  },
- 
-  inputView: {
-    backgroundColor: "#FFC0CB",
-    borderRadius: 30,
-    width: "70%",
-    height: 45,
-    marginBottom: 20,
- 
-    alignItems: "center",
-  },
- 
-  TextInput: {
-    height: 50,
+    margin: 20,
     flex: 1,
-    padding: 10,
-    marginLeft: 20,
-  },
- 
-  forgot_button: {
-    height: 30,
-    marginBottom: 30,
-  },
- 
-  loginBtn: {
-    width: "80%",
-    borderRadius: 25,
-    height: 50,
-    alignItems: "center",
-    justifyContent: "center",
-    marginTop: 40,
-    backgroundColor: "#FF1493",
   },
 });
