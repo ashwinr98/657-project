20881e0ea27f3db2cab3a7ba863d6a75e755ea95
unknown <vivekananda.marellali@gmail.com>
2022-06-14 08:44:54 +0530

    Firebase Configuration

---
+++ helpers/fb-credentials.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDed-r8e4B8ItKNvTq45hrn4UsAcBlfgKU",
+  authDomain: "semproject-9a86b.firebaseapp.com",
+  databaseURL: "https://semproject-9a86b-default-rtdb.firebaseio.com",
+  projectId: "semproject-9a86b",
+  storageBucket: "semproject-9a86b.appspot.com",
+  messagingSenderId: "575631994770",
+  appId: "1:575631994770:web:cf78c8cc2acf843d559b4b",
+  measurementId: "G-PY74XCV39D"
+};
